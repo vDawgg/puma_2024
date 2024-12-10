98534f8d55f140908bce2c115e8ba884829cbea7
veit <veit.laule@gmail.com>
2024-12-10 21:36:28 +0100

    Save masks instead of datasets; apply basic transform to ds.

--- make_ds.py
+++ make_ds.py
@@ -1,12 +1,22 @@
 from multiprocessing import Pool
 import os
+from typing import Any
 
-import torch
+import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image
+from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, ScaleIntensityd
 from wholeslidedata.annotation.labels import Label
 from wholeslidedata.samplers.patchlabelsampler import SegmentationPatchLabelSampler
 from wholeslidedata.annotation.wholeslideannotation import WholeSlideAnnotation
-from monai.data import Dataset
+from monai.data import Dataset, DataLoader
+import monai
+
+
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+geojson_dir = os.path.join(data_dir, "01_training_dataset_geojson_nuclei")
+ims_dir = os.path.join(data_dir, "01_training_dataset_tif_ROIs")
+masks_dir = os.path.join(data_dir, "masks_nuclei")
 
 def make_mask(geojson_path):
     tile_size = 1024
@@ -43,52 +53,65 @@ def make_mask(geojson_path):
             )
             return mask
 
-def get_ds(ims_path: str, segs_path: str, out_path: str, split=(0.8, 0.1, 0.1)) -> [Dataset]:
-    if not os.path.exists(os.path.join(out_path, "train.pt")):
-        pool = Pool()
+def get_maks(input_paths: [str], ppool: Any, output_path: str) -> [str]:
+    file_names = [os.path.join(output_path, input_path.split("/")[-1].split(".")[0] + ".tif") for input_path in input_paths]
 
-        metastatic_seg_paths = sorted([os.path.join(segs_path, seg_f) for seg_f in os.listdir(segs_path) if 'metastatic' in seg_f])
-        metastatic_img_paths = sorted([os.path.join(ims_path, im_f) for im_f in os.listdir(ims_path) if 'metastatic' in im_f])
-        primary_seg_paths = sorted([os.path.join(segs_path, seg_f) for seg_f in os.listdir(segs_path) if 'primary' in seg_f])
-        primary_img_paths = sorted([os.path.join(ims_path, im_f) for im_f in os.listdir(ims_path) if 'primary' in im_f])
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+        [Image.fromarray(mask).save(file_name) for mask, file_name in zip(ppool.map(make_mask, input_paths), file_names)]
 
-        train_split = int(len(metastatic_seg_paths)*split[0])
-        val_split = int(len(metastatic_seg_paths)*split[1])
+    return file_names
 
-        train_img_paths = metastatic_img_paths[:train_split] + primary_img_paths[:train_split]
-        train_seg_paths = metastatic_seg_paths[:train_split] + primary_seg_paths[:train_split]
-        val_img_paths = metastatic_img_paths[train_split:val_split] + primary_img_paths[train_split:val_split]
-        val_seg_paths = metastatic_seg_paths[train_split:val_split] + primary_seg_paths[train_split:val_split]
-        test_img_paths = metastatic_img_paths[train_split+val_split:] + primary_img_paths[train_split+val_split:]
-        test_seg_paths = metastatic_seg_paths[train_split+val_split:] + primary_seg_paths[train_split+val_split:]
+def get_ds(ims_path: str,
+           segs_path: str,
+           masks_path: str,
+           train_transforms: Compose,
+           val_test_transforms: Compose,
+           split=(0.8, 0.1, 0.1)) -> [Dataset]:
+    pool = Pool()
 
-        train_masks = pool.map(make_mask, train_seg_paths)
-        train_imgs = pool.map(Image.open, train_img_paths)
-        val_masks = pool.map(make_mask, val_seg_paths)
-        val_imgs = pool.map(Image.open, val_img_paths)
-        test_masks = pool.map(make_mask, test_seg_paths)
-        test_imgs = pool.map(Image.open, test_img_paths)
+    metastatic_seg_paths = sorted([os.path.join(segs_path, seg_f) for seg_f in os.listdir(segs_path) if 'metastatic' in seg_f])
+    metastatic_img_paths = sorted([os.path.join(ims_path, im_f) for im_f in os.listdir(ims_path) if 'metastatic' in im_f])
+    primary_seg_paths = sorted([os.path.join(segs_path, seg_f) for seg_f in os.listdir(segs_path) if 'primary' in seg_f])
+    primary_img_paths = sorted([os.path.join(ims_path, im_f) for im_f in os.listdir(ims_path) if 'primary' in im_f])
 
-        train_ds = Dataset([{"img": img, "seg": seg} for img, seg in zip(train_imgs, train_masks)])
-        val_ds = Dataset([{"img": img, "seg": seg} for img, seg in zip(val_imgs, val_masks)])
-        test_ds = Dataset([{"img": img, "seg": seg} for img, seg in zip(test_imgs, test_masks)])
+    train_split = int(len(metastatic_seg_paths)*split[0])
+    val_split = int(len(metastatic_seg_paths)*split[1])
 
-        torch.save(train_ds, os.path.join(out_path, "train_ds.pt"))
-        torch.save(val_ds, os.path.join(out_path, "val_ds.pt"))
-        torch.save(test_ds, os.path.join(out_path, "test_ds.pt"))
+    train_img_paths = metastatic_img_paths[:train_split] + primary_img_paths[:train_split]
+    train_seg_paths = metastatic_seg_paths[:train_split] + primary_seg_paths[:train_split]
+    val_img_paths = metastatic_img_paths[train_split:val_split] + primary_img_paths[train_split:val_split]
+    val_seg_paths = metastatic_seg_paths[train_split:val_split] + primary_seg_paths[train_split:val_split]
+    test_img_paths = metastatic_img_paths[train_split+val_split:] + primary_img_paths[train_split+val_split:]
+    test_seg_paths = metastatic_seg_paths[train_split+val_split:] + primary_seg_paths[train_split+val_split:]
 
-    else:
-        train_ds = torch.load(os.path.join(out_path, "train_ds.pt"))
-        val_ds = torch.load(os.path.join(out_path, "val_ds.pt"))
-        test_ds = torch.load(os.path.join(out_path, "test_ds.pt"))
+    train_mask_paths = get_maks(train_seg_paths, pool, os.path.join(masks_path, "train"))
+    val_mask_paths = get_maks(val_seg_paths, pool, os.path.join(masks_path, "val"))
+    test_mask_paths = get_maks(test_seg_paths, pool, os.path.join(masks_path, "test"))
+
+    train_ds = Dataset([{"img": img, "seg": seg} for img, seg in zip(train_img_paths, train_mask_paths)], transform=train_transforms)
+    val_ds = Dataset([{"img": img, "seg": seg} for img, seg in zip(val_img_paths, val_mask_paths)], transform=val_test_transforms)
+    test_ds = Dataset([{"img": img, "seg": seg} for img, seg in zip(test_img_paths, test_mask_paths)], transform=val_test_transforms)
 
     return train_ds, val_ds, test_ds
 
 
 if __name__ == "__main__":
-    ims_path = "./data/01_training_dataset_tif_ROIs"
-    segs_path = "./data/01_training_dataset_geojson_nuclei"
-    out_path = "./data/output"
+    base_transforms = Compose(
+        [
+            LoadImaged(keys=["img", "seg"]),
+            EnsureChannelFirstd(keys=["img", "seg"]),
+            ScaleIntensityd(keys=["img", "seg"]),
+        ]
+    )
 
-    train_ds, val_ds, test_ds = get_ds(ims_path, segs_path, out_path)
-    print(train_ds)
+    train_ds, val_ds, test_ds = get_ds(ims_dir, geojson_dir, masks_dir, base_transforms, base_transforms)
+
+    check_loader = DataLoader(train_ds)
+    check_data = monai.utils.misc.first(check_loader)
+    print(check_data["img"].shape, check_data["seg"].shape)
+
+    plt.imshow(check_data["img"][0, 0, :, :])
+    plt.show()
+    plt.imshow(np.transpose(check_data["seg"][0, :, :, :], (1, 2, 0)))
+    plt.show()
